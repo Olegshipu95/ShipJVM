15c0415c3f581aacbbdaef55314b9cc09f5d4e8e
Oleg Shipulin <olegshipu95@gmail.com>
2025-05-05 03:21:04 +0300

    added some instructions

--- src/runtime/bytecode/bytecode.c
+++ src/runtime/bytecode/bytecode.c
@@ -42,7 +42,15 @@ void opcode_aaload (struct stack_frame *frame);
 
 void opcode_aastore (struct stack_frame *frame);
 
-void opcode_aconst_null (struct stack_frame *frame);
+void
+opcode_aconst_null (struct stack_frame *frame)
+{
+  jvariable result = create_variable (JOBJECT);
+  if (opstack_push (frame->operand_stack, result))
+    {
+      prerr ("Stack overflow in ACONST_NULL");
+    }
+};
 
 void opcode_aload (struct stack_frame *frame, uint32_t index);
 
@@ -255,7 +263,40 @@ opcode_dneg (struct stack_frame *frame)
     }
 }
 
-void opcode_drem (struct stack_frame *frame);
+void
+opcode_drem (struct stack_frame *frame)
+{
+  jvariable value1, value2;
+
+  if (opstack_pop (frame->operand_stack, &value2)
+      || opstack_pop (frame->operand_stack, &value1))
+    {
+      prerr ("Stack underflow in DREM");
+      return;
+    }
+
+  if (check_var_type (&value1, JDOUBLE) || check_var_type (&value2, JDOUBLE))
+    {
+      prerr ("Type mismatch in DREM: expected double");
+      return;
+    }
+
+  if (value2.value._double == 0)
+    {
+      prerr ("ArithmeticException: division by zero in DREM"); // EXCEPTION
+      return;
+    }
+
+  jvariable result = create_variable (JDOUBLE);
+  result.value._double
+      = value1.value._double
+        - (value1.value._double / value2.value._double) * value2.value._double;
+
+  if (opstack_push (frame->operand_stack, result))
+    {
+      prerr ("Stack overflow in DREM");
+    }
+};
 
 void opcode_dreturn (struct stack_frame *frame);
 
@@ -315,13 +356,107 @@ opcode_dsub (struct stack_frame *frame)
     }
 }
 
-void opcode_dup (struct stack_frame *frame);
+void
+opcode_dup (struct stack_frame *frame)
+{
+  jvariable op1;
+  if (opstack_peek (frame->operand_stack, &op1))
+    {
+      prerr ("Stack underflow in DUP");
+      return;
+    }
 
-void opcode_dup_x1 (struct stack_frame *frame);
+  if (opstack_push (frame->operand_stack, op1))
+    {
+      prerr ("Stack overflow in DUP");
+    }
+};
 
-void opcode_dup_x2 (struct stack_frame *frame);
+void
+opcode_dup_x1 (struct stack_frame *frame)
+{
+  jvariable op1, op2;
 
-void opcode_dup2 (struct stack_frame *frame);
+  if (opstack_pop (frame->operand_stack, &op1)
+      || opstack_pop (frame->operand_stack, &op2))
+    {
+      prerr ("Stack underflow in DUP_X1");
+      return;
+    }
+
+  if (var_computation_type (&op1) == 1 || var_computation_type (&op2) == 1)
+    {
+      prerr ("Args are not category 1 computational type in DUP_X1");
+      return;
+    }
+
+  if (opstack_push (frame->operand_stack, op1)
+      || opstack_push (frame->operand_stack, op2)
+      || opstack_push (frame->operand_stack, op1))
+    {
+      prerr ("Stack overflow in DUP_X1");
+    }
+};
+
+void
+opcode_dup_x2 (struct stack_frame *frame)
+{
+  jvariable op1, op2, op3;
+  int computation_type;
+
+  if (opstack_pop (frame->operand_stack, &op1)
+      || opstack_pop (frame->operand_stack, &op2))
+    {
+      prerr ("Stack underflow in DUP_X2");
+      return;
+    }
+
+  if (var_computation_type (&op1) != 1)
+    {
+      prerr ("Value1 must be 1 computational type in DUP_X2");
+      return;
+    }
+
+  computation_type = var_computation_type (&op2);
+
+  if (computation_type == 1)
+    {
+      if (opstack_pop (frame->operand_stack, &op3))
+        {
+          prerr ("Stack underflow in DUP_X2");
+          return;
+        }
+      if (var_computation_type (&op3) == 1)
+        {
+          if (opstack_push (frame->operand_stack, op1)
+              || opstack_push (frame->operand_stack, op3)
+              || opstack_push (frame->operand_stack, op2)
+              || opstack_push (frame->operand_stack, op1))
+            {
+              prerr ("Stack overflow in DUP_X2");
+            }
+        }
+      else
+        {
+          prerr ("Value3 must be 1 computational type in DUP_X2");
+        }
+    }
+  else if (computation_type == 2)
+    {
+      if (opstack_push (frame->operand_stack, op1)
+          || opstack_push (frame->operand_stack, op2)
+          || opstack_push (frame->operand_stack, op1))
+        {
+          prerr ("Stack overflow in DUP_X2");
+        }
+    }
+  else
+    {
+      prerr ("Value2 undefined computational type in DUP_X2");
+    }
+}
+
+void opcode_dup2 (struct stack_frame *frame); // try to reuse
 
 void opcode_dup2_x1 (struct stack_frame *frame);
 
@@ -467,7 +602,40 @@ opcode_fmul (struct stack_frame *frame)
 //     }
 // }
 
-void opcode_frem (struct stack_frame *frame);
+void
+opcode_frem (struct stack_frame *frame)
+{
+  jvariable value1, value2;
+
+  if (opstack_pop (frame->operand_stack, &value2)
+      || opstack_pop (frame->operand_stack, &value1))
+    {
+      prerr ("Stack underflow in FREM");
+      return;
+    }
+
+  if (check_var_type (&value1, JFLOAT) || check_var_type (&value2, JFLOAT))
+    {
+      prerr ("Type mismatch in FREM: expected float");
+      return;
+    }
+
+  if (value2.value._int == 0)
+    {
+      prerr ("ArithmeticException: division by zero in FREM"); // EXCEPTION
+      return;
+    }
+
+  jvariable result = create_variable (JFLOAT);
+  result.value._float
+      = value1.value._float
+        - (value1.value._float / value2.value._float) * value2.value._float;
+
+  if (opstack_push (frame->operand_stack, result))
+    {
+      prerr ("Stack overflow in FREM");
+    }
+};
 void opcode_freturn (struct stack_frame *frame);
 
 void opcode_fstore (struct stack_frame *frame);
@@ -805,7 +973,41 @@ opcode_ior (struct stack_frame *frame)
     }
 }
 
-void opcode_irem (struct stack_frame *frame);
+void
+opcode_irem (struct stack_frame *frame)
+{
+  jvariable value1, value2;
+
+  if (opstack_pop (frame->operand_stack, &value2)
+      || opstack_pop (frame->operand_stack, &value1))
+    {
+      prerr ("Stack underflow in IREM");
+      return;
+    }
+
+  if (check_var_type (&value1, JINT) || check_var_type (&value2, JINT))
+    {
+      prerr ("Type mismatch in IREM: expected int");
+      return;
+    }
+
+  if (value2.value._int == 0)
+    {
+      prerr ("ArithmeticException: division by zero in IREM"); // EXCEPTION
+      return;
+    }
+
+  jvariable result = create_variable (JINT);
+  result.value._int
+      = value1.value._int
+        - (value1.value._int / value2.value._int) * value2.value._int;
+
+  if (opstack_push (frame->operand_stack, result))
+    {
+      prerr ("Stack overflow in IREM");
+    }
+}
+
 void opcode_ireturn (struct stack_frame *frame);
 void opcode_ishl (struct stack_frame *frame);
 void opcode_ishr (struct stack_frame *frame);
@@ -963,8 +1165,24 @@ opcode_land (struct stack_frame *frame)
 void opcode_lastore (struct stack_frame *frame);
 void opcode_lcmp (struct stack_frame *frame);
 
-void opcode_lconst_0 (struct stack_frame *frame);
-void opcode_lconst_1 (struct stack_frame *frame);
+void
+_common_lconst (struct stack_frame *frame, jlong value)
+{
+  jvariable var = create_variable (JLONG);
+  var.value._long = value;
+  opstack_push (frame->operand_stack, var);
+}
+
+void
+opcode_lconst_0 (struct stack_frame *frame)
+{
+  _common_lconst (frame, 0L);
+};
+void
+opcode_lconst_1 (struct stack_frame *frame)
+{
+  _common_lconst (frame, 1L);
+};
 
 void opcode_ldc (struct stack_frame *frame);
 void opcode_ldc_w (struct stack_frame *frame);
@@ -1117,6 +1335,32 @@ opcode_lrem (struct stack_frame *frame)
     {
       result.value._long += divisor.value._long;
     }
+
+  jvariable value1, value2;
+
+  if (opstack_pop (frame->operand_stack, &value2)
+      || opstack_pop (frame->operand_stack, &value1))
+    {
+      prerr ("Stack underflow in LREM");
+      return;
+    }
+
+  if (check_var_type (&value1, JLONG) || check_var_type (&value2, JLONG))
+    {
+      prerr ("Type mismatch in LREM: expected long");
+      return;
+    }
+
+  if (value2.value._long == 0)
+    {
+      prerr ("ArithmeticException: division by zero in LREM"); // EXCEPTION
+      return;
+    }
+
+  jvariable result = create_variable (JLONG);
+  result.value._long
+      = value1.value._long
+        - (value1.value._long / value2.value._long) * value2.value._long;
 
   if (opstack_push (frame->operand_stack, result))
     {
